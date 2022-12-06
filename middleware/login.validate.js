df5a3ce4b7a26ff70a09b0885b085dbb06ed1e36
Evgenia-cyber <kurineva85@mail.ru>
2022-12-06 21:33:45 +0300

    refactor: add email validation for login and registration

--- middleware/login.validate.js
+++ middleware/login.validate.js
@@ -1,6 +1,7 @@
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 const loginValidate = () => [
+  body('email').isEmail(),
   check('email', 'email не может быть пустым').notEmpty(),
   check('password', 'Пароль не может быть пустым').notEmpty(),
 ];
--- middleware/registration.validate.js
+++ middleware/registration.validate.js
@@ -1,10 +1,17 @@
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 const registrationValidate = () => [
+  body('email').isEmail(),
   check('email', 'email не может быть пустым').notEmpty(),
-  check('password', 'Пароль должен содержать от 7 до 10 символов').isLength({ min: 7, max: 10 }),
+  check('password', 'Пароль должен содержать от 7 до 10 символов').isLength({
+    min: 7,
+    max: 10,
+  }),
   check('fullName', 'ФИО пользователя не может быть пустым').notEmpty(),
-  check('registrationStartTime', 'Время регистрации не может быть пустым').notEmpty(),
+  check(
+    'registrationStartTime',
+    'Время регистрации не может быть пустым'
+  ).notEmpty(),
   check('registrationPeriod', 'Период оплаты не может быть пустым').notEmpty(),
   check('payment', 'Оплата не может быть пустой').notEmpty(),
 ];
