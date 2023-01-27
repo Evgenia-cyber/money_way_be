000a786ac56da86ef46c5859150893ad09826c68
Evgenia-cyber <kurineva85@mail.ru>
2023-01-27 18:11:08 +0300

    fix: use mode when writing cookie

--- utils/addTokens.js
+++ utils/addTokens.js
@@ -9,12 +9,23 @@ const addTokens = async (id, roles, res) => {
   // сохраняем refreshToken в БД
   await TokenUtil.saveToken(id, refreshToken);
 
+  const { MODE } = process.env;
+
   // записываем refreshToken в cookie:
-  res.cookie('refreshToken', refreshToken, {
-    maxAge: 30 * 24 * 60 * 60 * 1000, // кука, как и refreshToken, будет жить 30 дней
-    httpOnly: true, // чтобы cookie нельзя было изменять внутри браузера с JS
-    // secure: true, // TODO: для https - соединение должно быть установлено через HTTPS, иначе в cookie ничего не запишется
-  });
+  if (MODE === 'development') { // разработка
+    res.cookie('refreshToken', refreshToken, {
+      maxAge: 30 * 24 * 60 * 60 * 1000, // кука, как и refreshToken, будет жить 30 дней
+      httpOnly: true, // чтобы cookie нельзя было изменять внутри браузера с JS
+      sameSite: false,
+    });
+  } else {
+    res.cookie('refreshToken', refreshToken, { // продакшн
+      maxAge: 30 * 24 * 60 * 60 * 1000, // кука, как и refreshToken, будет жить 30 дней
+      httpOnly: true, // чтобы cookie нельзя было изменять внутри браузера с JS
+      sameSite: 'none',
+      secure: true, // для https - соединение должно быть установлено через HTTPS, иначе в cookie ничего не запишется
+    });
+  }
 
   return {
     accessToken,
